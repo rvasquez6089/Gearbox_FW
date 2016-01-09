27870b040b77deab9d06c5659de50693231eb5a1
rvasquez6089 <rvasquez6089@gmail.com>
2016-01-09 13:32:36 -0600

    Revert "poopoo"
    This reverts commit e340b605601b48820e05ca89269ab82c9a95b728.

--- PID_Control.h
+++ PID_Control.h
@@ -45,90 +45,37 @@ public:
 			float ki, //Integral Tuning Constant
 			float kd, //Derivative Tuning Constant
 			float bias); //optional Bias
-
-
 	/** Fill_XY_Buffer fills the X_data and Y_data arrays, this helps start 
 	 * the low pass filter and also fills the buffer when the constructor is
 	 * called. */
 	void Fill_XY_Buffer();
-
-
 	/** Reed_Acc reads data from the accelerometer and pushes the data into a
 	 * array buffer. It also filters the data with a first order Filter 
 	*/
 	void Read_Acc(); 
-
-
 	/** Fill_Angle_Buffer fills the Angle Buffer when the gear starts,
 	 * This helps to establish a good stable data base for the PID
 	 * control to base is starting calculations from
 	 */
 	void Fill_Angle_Buffer();
-
-
 	/** Read_Angle gets the angle and pushes into the Angle buffer
-	 * It calls the Read_Acc() function to bring in fresh accelerometer
-	 * data and it calculates the angle pushes it into the Angle data
-	 * buffer
 	 */
 	void Read_Angle();
 	
-
-	/**Fill_Angular_Spd_Buffer() fills the Angular_Spd buffer. This helps
-	 * in the PID_Init() function to fill the buffer with real valid data
-	 * from which to base its initial calculations and movements from.
-	 */
 	void Fill_Angular_Spd_Buffer();
 
-
-    /** Calc_Angular_Spd() calculates the angular speed from the most
-     * recent and second most recent angle values in the Angle buffer
-     * and pushes the angular speed into the Angular_Spd buffer.
-     */
 	void Calc_Angular_Spd();
 	
-
-	/** Clear_Error_Buffer() Clears the error buffer from the most
-	 * run of the gearbox. This helps the gearbox when starting up
-	 * to base its movement calculations from valid data.
-	 */
 	void Clear_Error_Buffer();
 
-
-    /**PID_Init() Calls the Fill_XY_Buffer(), Fill_Angle_Buffer(),
-     * Fill_Angular_Spd_Buffer, and Clear_Error_Buffer() functions to
-     * the PID loop all fresh valid data to start its PID calculations.
-     */
 	void PID_Init();
 
-
-	/**Calc_Error() function calculates the error by subtracting Trgt_Ang_Spd
-	 * from Angular_Spd[0] to get the error in angular speed. Once the
-	 * error is calculated it is then stored in the Error[] buffer.
-	 */
 	void Calc_Error();
 
-
-	/**Integrate_Error() integrates the error from the data stored in
-	 * Error buffer in respect to the sampling rate of the data and
-	 * length of the Error Buffer.
-	 */
 	float Integrate_Error();
 
-
-	/**Derivate_Error calculates the derivative of the error over a set
-	 * number of error sampels and returns a float of the change
-	 * in error over those samples.
-	 */
 	float Derivate_Error();
 
-	/**PID_Control() calls the Calc_Error() function which calculates
-	 * the current error in the angular speed of the gearbox
-	 * and calculates a new PWM value for the system and then
-	 * turns the motor according to those calculations
-	 * This function needs to be called on a constant period
-	 * basis (PID_update_period)
-	 */
 	void PID_Control();
 
 	void set_speed(float spd);
